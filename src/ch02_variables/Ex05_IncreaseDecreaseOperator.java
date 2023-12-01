da838c6ff004552bdd92db0d3520acde6bd4d19c
hongseokone <wjsrl20033@gmail.com>
2023-12-01 10:18:09 +0900

    증감연산자

---
+++ src/ch02_variables/Ex05_IncreaseDecreaseOperator.java
@@ -0,0 +1,29 @@
+package ch02_variables;
+
+public class Ex05_IncreaseDecreaseOperator {
+
+	public static void main(String[] args) {
+		// 증감연산자
+		int num1 = 10, num2 = 0, num3 = 0;
+		System.out.println("num1 : " + num1 + " num2: " + num2 + "num3: " + num3);
+		num2 = ++num1;
+		System.out.println("num1 : " + num1 + " num2: " + num2 + "num3: " + num3);
+		num2 = ++num1;
+		num3 = num1++;
+		System.out.println("num1 : " + num1 + " num2: " + num2 + "num3: " + num3);
+//		num1 = ++num3;
+//		num2 = num1++;
+//		System.out.println("num1 : " + num1 + " num2: " + num2 + "num3: " + num3);
+//		
+		
+		int result1 = num1++ + ++num2;
+		System.out.println("num1 : " + num1 + " num2: " + num2 + "num3: " + num3);
+		System.out.println(result1);
+		System.out.println("num1 : " + num1 + " num2: " + num2 + "num3: " + num3);
+		System.out.println(result1);
+		int result2 = --num1 + num2--;
+		System.out.println("num1 : " + num1 + " num2: " + num2 + "num3: " + num3);
+		System.out.println(result2);
+	}
+
+}

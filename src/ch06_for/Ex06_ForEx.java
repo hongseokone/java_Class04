eedd5c6924865effbf2364860bab6a576cf9911d
hongseokone <wjsrl20033@gmail.com>
2023-12-05 10:41:30 +0900

    for문 예제

--- src/ch06_for/Ex06_ForEx.java
+++ src/ch06_for/Ex06_ForEx.java
@@ -5,5 +5,14 @@ public class Ex06_ForEx {
         /**
          * 1부터 100까지의 숫자 중에서 3의 배수만 출력하고 합계도 구하기
          */
+        int f = 0;
+        int sum = 0;
+        for (f = 3; f <= 100; f+=3) {
+            System.out.println("f = " + f);
+        sum += f;
+        }
+        System.out.println(sum);
+
     }
 }
+

e0c06221b514bd1f64df06bbba87fcd2d49bd5dd
hongseokone <wjsrl20033@gmail.com>
2023-12-06 15:18:22 +0900

    while 예제 (평균)

---
+++ src/ch07_while/Ex06_whileEx.java
@@ -0,0 +1,36 @@
+package ch07_while;
+
+import java.util.Scanner;
+
+public class Ex06_whileEx {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // 숫자 입력용 변수
+        int num = 0;
+        // 합계용 변수
+        int sum = 0;
+        // 갯수용 변수
+        int count = 0;
+        boolean run = true;
+        System.out.println("정수를 입력하고 마지막에 0을 입력해주세요 : ");
+        while (run) {
+            num = scanner.nextInt();
+//            System.out.println("num = " + num);
+            if (num == 0) {
+                run = false;
+            } else {
+                // 합계계산
+                sum += num;
+                // 입력된 숫자 갯수 +1
+                count++;
+            }
+        }
+        int gvg1 = sum / count;
+        System.out.println("gvg1 = " + gvg1);
+        double avg2 = sum / count;
+        System.out.println("avg2 = " + avg2);
+        double avg3 = (double) sum / count;
+        System.out.println("avg3 = " + avg3);
+    }
+}
+
